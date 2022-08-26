404106c8dba9231a4c27bf37e730d660006c3e00
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-26 02:51:07 +0000

    modified getAllReservations function to use sql

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -78,7 +78,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+      SELECT reservations.*, properties.*, AVG(ratings) AS average_rating
+      FROM properties 
+      JOIN reservations on reservations.property_id = properties.id
+      JOIN property_reviews ON property_reviews.property_id = properties.id
+      WHERE property_reviews.guest_id = $1
+      GROUP BY reservations.id, properties.id
+      ORDER BY start_date
+      LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
